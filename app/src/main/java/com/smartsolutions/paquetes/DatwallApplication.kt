80f4c471d4c6d54158f22968035831d118ca414b
Abel <lleraabi@gmail.com>
2021-03-23 12:25:12 -0400

    Initial configuration

--- app/src/main/java/com/smartsolutions/paquetes/DatwallApplication.kt
+++ app/src/main/java/com/smartsolutions/paquetes/DatwallApplication.kt
@@ -1,4 +1,7 @@
 package com.smartsolutions.paquetes
 
-class DatwallApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DatwallApplication : Application()
--- app/src/main/java/com/smartsolutions/paquetes/MainActivity.kt
+++ app/src/main/java/com/smartsolutions/paquetes/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
