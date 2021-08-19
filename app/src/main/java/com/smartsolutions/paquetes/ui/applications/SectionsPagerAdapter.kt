c3e1908fe6b45b75c4e6ef1d432523c89ac5957c
Abel <lleraabi@gmail.com>
2021-08-19 09:43:28 -0400

    Correcciones de errores

--- app/src/main/java/com/smartsolutions/paquetes/ui/applications/SectionsPagerAdapter.kt
+++ app/src/main/java/com/smartsolutions/paquetes/ui/applications/SectionsPagerAdapter.kt
@@ -1,38 +1,28 @@
 package com.smartsolutions.paquetes.ui.applications
 
-import android.content.Context
 import androidx.annotation.StringDef
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.smartsolutions.paquetes.R
 
 class SectionsPagerAdapter(
-    context: Context,
-    fragmentManager: FragmentManager
-) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    fragment: Fragment
+) : FragmentStateAdapter(fragment) {
 
-    private val pages = arrayOf(
-        Pair(USER_APPS, context.getString(R.string.page_user_apps)),
-        Pair(SYSTEM_APPS, context.getString(R.string.page_system_apps))
+    val pages = arrayOf(
+        Pair(USER_APPS, fragment.getString(R.string.page_user_apps)),
+        Pair(SYSTEM_APPS, fragment.getString(R.string.page_system_apps))
     )
-
-    override fun getCount(): Int {
-        return pages.size
-    }
-
-    override fun getItem(position: Int): Fragment {
-        return PlaceHolderFragment.newInstance(pages[position].first)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return pages[position].second
-    }
 
     companion object {
         const val USER_APPS = "user_apps"
         const val SYSTEM_APPS = "system_apps"
     }
+
+    override fun getItemCount() = pages.size
+
+    override fun createFragment(position: Int): Fragment =
+        PlaceHolderFragment.newInstance(pages[position].first)
 }
 
 @StringDef(SectionsPagerAdapter.USER_APPS, SectionsPagerAdapter.SYSTEM_APPS)
