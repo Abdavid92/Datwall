92ca342e774a0c115f483564bd4e4f615c813512
Abdavid92 <lleraabi@gmail.com>
2021-09-18 23:00:15 -0400

    Trabajando en el DashboardFragment.kt

--- app/src/main/java/com/smartsolutions/paquetes/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/smartsolutions/paquetes/ui/dashboard/DashboardFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.button.MaterialButton
 import com.smartsolutions.paquetes.ui.ApplicationFragment
 import com.smartsolutions.paquetes.R
 import com.smartsolutions.paquetes.annotations.ApplicationStatus
+import com.smartsolutions.paquetes.databinding.FragmentDashboardBinding
 import com.smartsolutions.paquetes.helpers.USSDHelper
 import com.smartsolutions.paquetes.managers.contracts.IDataPackageManager
 import com.smartsolutions.paquetes.serverApis.models.AndroidApp
@@ -23,41 +25,28 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class DashboardFragment : ApplicationFragment() {
+class DashboardFragment : Fragment() {
 
     private val dashboardViewModel by viewModels<DashboardViewModel>()
 
-    @Inject
-    lateinit var ussdHelper: USSDHelper
-
-    @Inject
-    lateinit var dataPackageManager: IDataPackageManager
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
+        binding = FragmentDashboardBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val textView: TextView = view.findViewById(R.id.text_dashboard)
-
-        dashboardViewModel.apps.observe(viewLifecycleOwner, {
-            textView.text = "Cantidad de aplicaciones: ${it.size}"
-        })
-    }
-    
-    private fun deleteOne(view: View) {
-        SinglePermissionFragment.newInstance(34)
-            .show(parentFragmentManager, "Single")
-    }
-
-    private fun testDownload() {
-
 
     }
 }
