138be62718fa4e3457106d89b2c49677ae59df5e
Abdavid92 <lleraabi@gmail.com>
2021-10-05 12:15:24 -0400

    Correciones menores

--- app/src/main/java/com/smartsolutions/paquetes/ui/packages/PackagesHolderFragment.kt
+++ app/src/main/java/com/smartsolutions/paquetes/ui/packages/PackagesHolderFragment.kt
@@ -1,6 +1,5 @@
 package com.smartsolutions.paquetes.ui.packages
 
-import android.app.AlertDialog
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import com.smartsolutions.paquetes.R
 import com.smartsolutions.paquetes.databinding.FragmentPackagesHolderBinding
