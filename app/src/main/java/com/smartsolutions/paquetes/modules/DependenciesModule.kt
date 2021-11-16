da7bf6e409fb74b123d6499a545d2355470b0c67
Abel David <lleraabi@gmail.com>
2021-11-16 15:58:59 -0500

    Corrección de errores y depuración

--- app/src/main/java/com/smartsolutions/paquetes/modules/DependenciesModule.kt
+++ app/src/main/java/com/smartsolutions/paquetes/modules/DependenciesModule.kt
@@ -1,9 +1,12 @@
 package com.smartsolutions.paquetes.modules
 
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
