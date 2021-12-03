93a5f1c553923bd77cd017fec25c9b277098db6a
EJV96 <apps.smartsolutions.cuba@gmail.com>
2021-12-03 13:23:07 -0500

    Added simply ControlsWidget.kt pending to UI fixes

---
+++ app/src/main/java/com/smartsolutions/paquetes/ui/widgets/ControlsWidget.kt
@@ -0,0 +1,169 @@
+package com.smartsolutions.paquetes.ui.widgets
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import com.smartsolutions.paquetes.R
+import com.smartsolutions.paquetes.helpers.BubbleServiceHelper
+import com.smartsolutions.paquetes.helpers.FirewallHelper
+import com.smartsolutions.paquetes.helpers.UIHelper
+import com.smartsolutions.paquetes.ui.MainActivity
+import dagger.Lazy
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ControlsWidget : AppWidgetProvider() {
+
+    @Inject
+    lateinit var bubbleHelper: Lazy<BubbleServiceHelper>
+
+    @Inject
+    lateinit var firewallHelper: Lazy<FirewallHelper>
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            appWidgetManager.updateAppWidget(appWidgetId, getRemotesViews(context))
+        }
+    }
+
+    override fun onEnabled(context: Context) {}
+    override fun onDisabled(context: Context) {}
+
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        var isAction = true
+
+        when (intent?.action) {
+            START_BUBBLE -> {
+                runBlocking {
+                    bubbleHelper.get().startBubble(true)
+                }
+            }
+            STOP_BUBBLE -> {
+                runBlocking {
+                    bubbleHelper.get().stopBubble(true)
+                }
+            }
+            START_FIREWALL -> {
+                runBlocking {
+                    firewallHelper.get().startFirewall(true)
+                }
+            }
+            STOP_FIREWALL -> {
+                runBlocking {
+                    firewallHelper.get().stopFirewall(true)
+                }
+            }
+            else -> {
+                isAction = false
+            }
+        }
+
+        if (isAction) {
+            context?.let { ctx ->
+                AppWidgetManager.getInstance(ctx)
+                    .updateAppWidget(
+                        ComponentName(
+                            ctx,
+                            ControlsWidget::class.java
+                        ),
+                        getRemotesViews(ctx)
+                    )
+            }
+        }
+
+    }
+
+    private fun getRemotesViews(context: Context): RemoteViews {
+        val views = RemoteViews(context.packageName, R.layout.widget_controls)
+
+        val isBubble = runBlocking {
+            bubbleHelper.get().bubbleEnabled()
+        }
+
+        val isFirewall = runBlocking {
+            firewallHelper.get().firewallEnabled()
+        }
+
+        views.setImageViewResource(
+            R.id.icon_firewall,
+            if (isFirewall) {
+                R.drawable.ic_firewall
+            } else {
+                R.drawable.ic_firewall_off_24
+            }
+        )
+
+        views.setImageViewResource(
+            R.id.icon_bubble,
+            if (isBubble) {
+                R.drawable.ic_bubble
+            } else {
+                R.drawable.ic_bubble_off_24
+            }
+        )
+
+        views.setOnClickPendingIntent(
+            R.id.lin_firewall,
+            getPendingSelfIntent(
+                context,
+                if (isFirewall) {
+                    STOP_FIREWALL
+                } else {
+                    START_FIREWALL
+                }
+            )
+        )
+
+        views.setOnClickPendingIntent(
+            R.id.lin_bubble,
+            getPendingSelfIntent(
+                context,
+                if (isBubble) {
+                    STOP_BUBBLE
+                } else {
+                    START_BUBBLE
+                }
+            )
+        )
+
+        val uiHelper = UIHelper(context)
+
+        //TODO Set Colors and Backgrounds according to theme
+
+        return views
+    }
+
+
+    private fun getPendingSelfIntent(context: Context, action: String): PendingIntent {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+        return PendingIntent.getBroadcast(
+            context, 0, intent, if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+            } else {
+                PendingIntent.FLAG_UPDATE_CURRENT
+            }
+        )
+    }
+
+    companion object {
+        const val START_BUBBLE = "start_bubble"
+        const val STOP_BUBBLE = "stop_bubble"
+        const val START_FIREWALL = "start_firewall"
+        const val STOP_FIREWALL = "stop_firewall"
+    }
+}
