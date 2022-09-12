f26c20a11f5680088e18c22788044d434e525c9c
Abdavid92 <69399613+Abdavid92@users.noreply.github.com>
2022-09-11 23:24:02 -0400

    Delete apklisupdate directory

--- apklisupdate/src/main/java/cu/uci/apklisupdate/model/ApiResponce.kt
+++
@@ -1,8 +0,0 @@
-package cu.uci.apklisupdate.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ApiResponce(
-    @SerializedName("count") val count: Int = 0,
-    @SerializedName("results") val results: List<AppUpdateInfo> = listOf()
-)
